b4c7bfc30dd278a8ee190ecd82fc9c593669efcc
wujiahao <wujiahao@coocaa.com>
2025-08-06 19:11:47 +0800

    init: 提交仓库

---
+++ src/main/java/com/learn/spring/SpringawareinterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringawareinterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringawareinterfaceApplication.class, args);
+    }
+
+}
